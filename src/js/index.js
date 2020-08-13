7c96a6a99b81fb13911eec22f27c536d67ddd41c
nekac <nikola.nenin91@gmail.com>
2020-08-13 14:04:48 +0200

    Updating servings and counters + Missing file added back

--- src/js/index.js
+++ src/js/index.js
@@ -91,3 +91,16 @@ const controlRecipe = async () => {
 ["hashchange", "load"].forEach((event) =>
   window.addEventListener(event, controlRecipe)
 );
+
+// handling recepie button clicks, delegation
+elements.recipe.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec");
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
+    state.recipe.updateServings("inc");
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+});
